c1b1989bdef92ace0fb8ee1b898c4ac38f581fcd
Lazaro <joselazaroneto97@gmail.com>
2023-01-18 11:26:59 -0300

    Adding stock and price columns

--- app/models.py
+++ app/models.py
@@ -3,4 +3,6 @@ from django.db import models
 
 class Products(models.Model):
     name = models.CharField(max_length=200)
+    stock = models.IntegerField(default=0)
+    price = models.FloatField(default=0.0)
     created_at = models.DateTimeField(auto_now_add=True)
