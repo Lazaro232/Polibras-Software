19eaf9bdf8d795611c3bc2481be4b9216649fa71
Lazaro <joselazaroneto97@gmail.com>
2023-01-18 15:40:24 -0300

    Adding router registration

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from app.api import viewsets
+
+route = routers.DefaultRouter()
+route.register(r'products', viewsets.ProductViewSet)
+route.register(r'payments', viewsets.PaymentViewSet)
+route.register(r'sales', viewsets.SaleViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/', include('app.urls')),
-    # path('products/', include('app.urls')),
-    path('products/create/', include('app.urls')),
+    path('', include(route.urls)),
 ]
