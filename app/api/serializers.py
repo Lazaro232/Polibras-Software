34ba6e80016df5ac244c545efef3f248e31db4ea
Lazaro <joselazaroneto97@gmail.com>
2023-01-18 15:41:05 -0300

    Adding payment and sale serializers

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -2,7 +2,20 @@ from app import models
 from rest_framework import serializers
 
 
-class ProductsSerializer(serializers.ModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Products
         fields = '__all__'
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Payments
+        fields = '__all__'
+
+
+class SaleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Sales
+        fields = '__all__'
+        # fields = ['product', 'payment', 'quantity_sold']
