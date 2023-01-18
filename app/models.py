3ba2f497b161d2bc80a64c2b927b4d06ef5b1157
Lazaro <joselazaroneto97@gmail.com>
2023-01-18 15:41:33 -0300

    Adding payment and sale models

--- app/models.py
+++ app/models.py
@@ -2,7 +2,19 @@ from django.db import models
 
 
 class Products(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=150)
     stock = models.IntegerField(default=0)
-    price = models.FloatField(default=0.0)
+    price = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Payments(models.Model):
+    method = models.CharField(max_length=200)
+
+
+class Sales(models.Model):
+    product = models.ForeignKey(Products, on_delete=models.PROTECT)
+    payment = models.ForeignKey(Payments, on_delete=models.PROTECT)
+    quantity_sold = models.IntegerField()
+    amount_sold = models.FloatField(default=0.0)
+    date = models.DateTimeField(auto_now_add=True)
