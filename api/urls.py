32bbf06f6d0cd9b31c7075ca2be0241609f2b005
Lazaro <joselazaroneto97@gmail.com>
2023-01-19 13:24:10 -0300

    Adding Swagger urls

--- api/urls.py
+++ api/urls.py
@@ -4,15 +4,29 @@ from django.urls import path, include
 from rest_framework import routers
 from rest_framework_simplejwt.views import (
     TokenObtainPairView, TokenRefreshView)
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 
+# Routes
 route = routers.DefaultRouter()
 route.register(r'products', viewsets.ProductViewSet)
 route.register(r'payments', viewsets.PaymentViewSet)
 route.register(r'sales', viewsets.SaleViewSet)
-
+# Swagger schema view
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Sales API",
+        default_version="1.0.0",
+        description="API documentation of App"
+    ),
+    public=True
+)
+# Urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('token/', TokenObtainPairView.as_view()),
     path('token/refresh/', TokenRefreshView.as_view()),
+    path('swagger/schema/', schema_view.with_ui('swagger',
+         cache_timeout=0), name="swagger-schema"),
     path('', include(route.urls)),
 ]
